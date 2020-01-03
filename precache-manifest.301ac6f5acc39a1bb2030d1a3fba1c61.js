411a34744db60e6161e23a555cf12a706cc03360
Andrii <farikpite@gmail.com>
2020-01-03 02:20:45 +0200

    Updates

---
+++ precache-manifest.301ac6f5acc39a1bb2030d1a3fba1c61.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "424b6cc02319bef0a666aed31dedbb71",
+    "url": "/react_airport/index.html"
+  },
+  {
+    "revision": "11466bcbe5ae196be852",
+    "url": "/react_airport/static/css/main.d86be4e2.chunk.css"
+  },
+  {
+    "revision": "4e30f3413811f8d2cdfd",
+    "url": "/react_airport/static/js/2.983c3ddf.chunk.js"
+  },
+  {
+    "revision": "11466bcbe5ae196be852",
+    "url": "/react_airport/static/js/main.62058863.chunk.js"
+  },
+  {
+    "revision": "c8cd8e70b1c2d1cd9765",
+    "url": "/react_airport/static/js/runtime~main.037fa8df.js"
+  }
+]);
